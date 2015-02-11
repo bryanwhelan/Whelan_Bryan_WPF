91c280a9536fe4d6d4dd11fbc848866876b3010f
bryanwhelan <bryanwhelan48@gmail.com>
2015-02-10 22:22:59 -0800

    Values and Variables
    assignment

---
+++ Variables Boolean and Strings/js/script.js
@@ -0,0 +1,9 @@
+//Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 30; //Number Variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Bryan";
+
+var phrase = 'I don\'t know \n You\'ll have to ask your professor';
