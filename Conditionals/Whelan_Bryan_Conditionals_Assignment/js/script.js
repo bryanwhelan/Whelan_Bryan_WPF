d1c0364e10911e8d21f889eef2e227a9f550a68d
bryanwhelan <bryanwhelan48@gmail.com>
2015-02-25 20:23:16 -0800

    Functions
    Assignment

--- Conditionals/Whelan_Bryan_Conditionals_Assignment/js/script.js
+++ Conditionals/Whelan_Bryan_Conditionals_Assignment/js/script.js
@@ -7,10 +7,10 @@ alert("To determine if you're eligible to compete in the skateboard contest plea
 //Variables
 var vertTricks = prompt("Please enter how many vert tricks you can do:");
 var techTricks = prompt("Please enter how many tech tricks can you do:");
-var sponsors = prompt("Please enter number of sponsors you have:");
+var sponsors = prompt("Please enter the number of sponsors you have:");
 //if the total number of tricks combined are greater than ten you may compete.
 if(Number(vertTricks) + Number(techTricks) > 10){
-    console.log("Congratulations! You may compete in the skateboard contest!");
+    console.log ("Congratulations! You may compete in the skateboard contest!");
 //if the total number of tricks are less than ten but, you have at least one sponsor you may compete.
 }else if (Number(vertTricks) + Number(techTricks) < 10 && Number(sponsors) >= 1){
     console.log("You may not have the tricks but you got the sponsors! \nCongratulations you may compete in the contest!")
---
+++ Functions/Returning Values/js/script.js
@@ -0,0 +1,9 @@
+//Functions - Returning Values
+
+var total = calcArea(30,20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; //function spitting the info out
+}
+console.log(total);
